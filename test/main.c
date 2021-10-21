7683582539fc862f0d980ef26a216b02bb5b310e
wanghang697 <wanghang20031106@163.com>
2021-10-21 19:02:29 +0800

    Initial Commit

---
+++ test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test
+//
+//  Created by 汪航 on 2021/10/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
