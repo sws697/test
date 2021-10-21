cd248c00612f0628d047a4ff9c9b1f24f99aaad9
wanghang697 <wanghang20031106@163.com>
2021-10-21 19:41:52 +0800

    add a function

--- test/main.c
+++ test/main.c
@@ -6,7 +6,19 @@
 //
 
 #include <stdio.h>
-
+int chprime(int t)
+{
+    int isprime1=1;
+    for (int j=2;j<t;++j)
+    {
+        if (t%j==0)
+        {
+            isprime1=0;
+            break;
+        }
+    }
+    return isprime1;
+}
 int main(int argc, const char * argv[]) {
     // insert code here...
     printf("Hello, World!\n");
